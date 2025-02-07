2a5fdacc35f3f785c36b3ea6a340e51de1de2a35
KaiWen <yuepaiji@163.com>
2025-02-07 12:36:49 +0800

    add useragent

--- useragent/useragent.go
+++ useragent/useragent.go
@@ -3,6 +3,7 @@ package useragent
 import (
 	"regexp"
 	"strings"
+	"sync"
 )
 
 // 预编译正则表达式以提高性能
@@ -13,6 +14,61 @@ var (
 	edgeRegex    = regexp.MustCompile(`(?i)edge\/[\d.]+`)
 	operaRegex   = regexp.MustCompile(`(?i)opr\/[\d.]+`)
 	ieRegex      = regexp.MustCompile(`(?i)msie [\d.]+|trident\/[\d.]+`)
+
+	// 使用 map 存储常见的爬虫标识，提高查找效率
+	botIdentifiers = map[string]bool{
+		"bot":                true,
+		"spider":            true,
+		"crawler":           true,
+		"slurp":             true,
+		"bingbot":           true,
+		"baiduspider":       true,
+		"yandexbot":         true,
+		"sogou":             true,
+		"exabot":            true,
+		"facebookexternalhit": true,
+		"ia_archiver":       true,
+	}
+
+	// 使用 map 存储常见浏览器标识，提高查找效率
+	commonBrowserIdentifiers = map[string]bool{
+		"mozilla":   true,
+		"webkit":    true,
+		"gecko":     true,
+		"presto":    true,
+		"trident":   true,
+		"chrome":    true,
+		"safari":    true,
+		"firefox":   true,
+		"edge":      true,
+		"opera":     true,
+		"msie":      true,
+		"opr":       true,
+		"mobile":    true,
+		"android":   true,
+		"iphone":    true,
+		"ipad":      true,
+		"ipod":      true,
+		"samsung":   true,
+		"miui":      true,
+		"ucbrowser": true,
+		"qqbrowser": true,
+		"maxthon":   true,
+		"crios":     true,
+		"fxios":     true,
+	}
+)
+
+// 缓存结构
+type cache struct {
+	sync.RWMutex
+	data map[string]interface{}
+}
+
+// 创建缓存实例
+var (
+	isBrowserCache = &cache{data: make(map[string]interface{})}
+	browserInfoCache = &cache{data: make(map[string]interface{})}
 )
 
 // BrowserInfo 存储浏览器信息
@@ -28,39 +84,42 @@ func IsBrowser(userAgent string) bool {
 		return false
 	}
 
+	// 检查缓存
+	isBrowserCache.RLock()
+	if result, ok := isBrowserCache.data[userAgent].(bool); ok {
+		isBrowserCache.RUnlock()
+		return result
+	}
+	isBrowserCache.RUnlock()
+
 	ua := strings.ToLower(userAgent)
 
 	// 检查是否为爬虫或机器人
-	if strings.Contains(ua, "bot") ||
-		strings.Contains(ua, "spider") ||
-		strings.Contains(ua, "crawler") ||
-		strings.Contains(ua, "slurp") ||
-		strings.Contains(ua, "bingbot") ||
-		strings.Contains(ua, "baiduspider") ||
-		strings.Contains(ua, "yandexbot") ||
-		strings.Contains(ua, "sogou") ||
-		strings.Contains(ua, "exabot") ||
-		strings.Contains(ua, "facebookexternalhit") ||
-		strings.Contains(ua, "ia_archiver") {
-		return false
-	}
-
-	// 检查常见浏览器引擎和标识
-	commonBrowsers := []string{
-		"mozilla", "webkit", "gecko", "presto", "trident", // 浏览器引擎
-		"chrome", "safari", "firefox", "edge", "opera", "msie", "opr", // 桌面浏览器
-		"mobile", "android", "iphone", "ipad", "ipod", // 移动端浏览器
-		"samsung", "miui", "ucbrowser", "qqbrowser", "maxthon", // 其他常见浏览器
-		"crios", "fxios", // iOS 上的 Chrome 和 Firefox
+	for bot := range botIdentifiers {
+		if strings.Contains(ua, bot) {
+			// 存入缓存
+			isBrowserCache.Lock()
+			isBrowserCache.data[userAgent] = false
+			isBrowserCache.Unlock()
+			return false
+		}
 	}
 
 	// 检查是否包含任一浏览器标识
-	for _, browser := range commonBrowsers {
+	for browser := range commonBrowserIdentifiers {
 		if strings.Contains(ua, browser) {
+			// 存入缓存
+			isBrowserCache.Lock()
+			isBrowserCache.data[userAgent] = true
+			isBrowserCache.Unlock()
 			return true
 		}
 	}
 
+	// 存入缓存
+	isBrowserCache.Lock()
+	isBrowserCache.data[userAgent] = false
+	isBrowserCache.Unlock()
 	return false
 }
 
@@ -70,30 +129,51 @@ func GetBrowserInfo(userAgent string) BrowserInfo {
 		return BrowserInfo{IsBrowser: false}
 	}
 
+	// 检查缓存
+	browserInfoCache.RLock()
+	if result, ok := browserInfoCache.data[userAgent].(BrowserInfo); ok {
+		browserInfoCache.RUnlock()
+		return result
+	}
+	browserInfoCache.RUnlock()
+
 	// 检查是否为爬虫或机器人
-	if strings.Contains(strings.ToLower(userAgent), "bot") ||
-		strings.Contains(strings.ToLower(userAgent), "spider") ||
-		strings.Contains(strings.ToLower(userAgent), "crawler") {
-		return BrowserInfo{IsBrowser: false}
+	ua := strings.ToLower(userAgent)
+	for bot := range botIdentifiers {
+		if strings.Contains(ua, bot) {
+			result := BrowserInfo{IsBrowser: false}
+			// 存入缓存
+			browserInfoCache.Lock()
+			browserInfoCache.data[userAgent] = result
+			browserInfoCache.Unlock()
+			return result
+		}
 	}
 
 	// 按优先级检查浏览器类型
+	var result BrowserInfo
 	switch {
 	case edgeRegex.MatchString(userAgent):
-		return extractBrowserInfo(userAgent, "Edge", edgeRegex)
+		result = extractBrowserInfo(userAgent, "Edge", edgeRegex)
 	case operaRegex.MatchString(userAgent):
-		return extractBrowserInfo(userAgent, "Opera", operaRegex)
+		result = extractBrowserInfo(userAgent, "Opera", operaRegex)
 	case chromeRegex.MatchString(userAgent) && !edgeRegex.MatchString(userAgent):
-		return extractBrowserInfo(userAgent, "Chrome", chromeRegex)
+		result = extractBrowserInfo(userAgent, "Chrome", chromeRegex)
 	case firefoxRegex.MatchString(userAgent):
-		return extractBrowserInfo(userAgent, "Firefox", firefoxRegex)
+		result = extractBrowserInfo(userAgent, "Firefox", firefoxRegex)
 	case ieRegex.MatchString(userAgent):
-		return extractBrowserInfo(userAgent, "Internet Explorer", ieRegex)
+		result = extractBrowserInfo(userAgent, "Internet Explorer", ieRegex)
 	case safariRegex.MatchString(userAgent) && !chromeRegex.MatchString(userAgent):
-		return extractBrowserInfo(userAgent, "Safari", safariRegex)
+		result = extractBrowserInfo(userAgent, "Safari", safariRegex)
 	default:
-		return BrowserInfo{IsBrowser: IsBrowser(userAgent)}
+		result = BrowserInfo{IsBrowser: IsBrowser(userAgent)}
 	}
+
+	// 存入缓存
+	browserInfoCache.Lock()
+	browserInfoCache.data[userAgent] = result
+	browserInfoCache.Unlock()
+	return result
 }
 
 // extractBrowserInfo 从User-Agent中提取浏览器版本信息
